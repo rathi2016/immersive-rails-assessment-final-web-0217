c0d2a6d91b9efee3d8e81eb95a6c6b66afab3c2d
github username <github email address>
2017-03-27 14:50:20 -0400

    done

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -4,10 +4,24 @@ class AppearancesController < ApplicationController
     @appearance = Appearance.new
   end
 
+
+  def edit
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def update
+
+    @appearance = Appearance.find(params[:id])
+    @appearance.update(appearance_params)
+    redirect_to new_appearance_path
+  end
   def create
     @appearance = Appearance.new(appearance_params)
+    @appearance.user_id = session[:user_id]
+
     if @appearance.save
       redirect_to @appearance.episode
+
     else
       redirect_to new_appearance_path
     end
@@ -16,7 +30,7 @@ class AppearancesController < ApplicationController
   private
 
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(:guest_id, :episode_id,:user_id,:rating)
   end
 
 end
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,5 +1,7 @@
 class Appearance < ApplicationRecord
   belongs_to :guest
   belongs_to :episode
-  
+  belongs_to :user
+
+  validates_inclusion_of :rating, :in => 1..5
 end
